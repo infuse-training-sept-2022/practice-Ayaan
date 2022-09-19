5db2eb642177c400449583482b468f7df2048505
ayaanshaikh0187 <ayaan.shaikhtks@gmail.com>
2022-09-19 16:14:06 +0530

    Intial commit & added calc.cs file

---
+++ calc.cs
@@ -0,0 +1,10 @@
+using System;
+	public class calc
+	{
+    		public static int Sum(int num1, int num2)
+    		{
+        		int total;
+        		total = num1 + num2;
+        		return total;
+		}
+    	}
